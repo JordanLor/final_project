99a3263b1bd03bcc0d982b05ef14b7a40497370c
Anthony Burch <aburch@gmail.com>
2014-01-22 23:48:37 -0500

    Swipe_Fragments added

---
+++ Swipe_Fragments/src/com/example/swipe_fragments/Fragment4.java
@@ -0,0 +1,16 @@
+package com.example.swipe_fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class Fragment4 extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return (LinearLayout) inflater.inflate(R.layout.fragment4, container, false);
+	}
+	
+}
---
+++ Swipe_Fragments/src/com/example/swipe_fragments/PageAdapter.java
@@ -0,0 +1,29 @@
+package com.example.swipe_fragments;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter{
+
+	private List<Fragment> fragments;
+	
+	public PageAdapter(FragmentManager fm, List<Fragment> fragments) {
+		super(fm);
+		this.fragments = fragments;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		return fragments.get(position);
+	}
+
+	@Override
+	public int getCount() {
+		return fragments.size();
+	}
+	
+
+}
