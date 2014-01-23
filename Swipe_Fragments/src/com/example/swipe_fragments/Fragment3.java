22bf34de4a8a215399d9dcc18f80fc40617f0ff5
Anthony Burch <aburch@gmail.com>
2014-01-23 14:12:35 -0500

    Added basic camera preview to Fragment 3

--- Swipe_Fragments/src/com/example/swipe_fragments/Fragment3.java
+++ Swipe_Fragments/src/com/example/swipe_fragments/Fragment3.java
@@ -1,125 +1,93 @@
 package com.example.swipe_fragments;
 
+import java.io.IOException;
+
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class Fragment3 extends Fragment {
+public class Fragment3 extends Fragment implements SurfaceHolder.Callback{
 	
-	private SurfaceView preview = null;
-	private SurfaceHolder previewHolder = null;
-	private Camera camera = null;
-	private boolean inPreview = false;
-	private boolean cameraConfigured = false;
+	Camera camera;
+	SurfaceView surfaceView;
+	SurfaceHolder surfaceHolder;
+	boolean previewing = false;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
-		//preview = (SurfaceView) getView().findViewById(R.id.preview);
-		//previewHolder = preview.getHolder();
-		//previewHolder.addCallback(surfaceCallback);
-		//previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		View view = inflater.inflate(R.layout.fragment3, null);
 		
-		return (LinearLayout) inflater.inflate(R.layout.fragment3, container, false);
-	
+		Button buttonStartCameraPreview = (Button) view.findViewById(R.id.startcamerapreview);
+		Button buttonStopCameraPreview = (Button) view.findViewById(R.id.stopcamerapreview);
+		
+		surfaceView = (SurfaceView) view.findViewById(R.id.surfaceview);
+		surfaceHolder = surfaceView.getHolder();
+		surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
+		//Start Preview Button Listener
+		buttonStartCameraPreview.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(!previewing) {
+					camera = camera.open();
+					if (camera != null) {
+						try {
+							camera.setPreviewDisplay(surfaceHolder);
+							camera.setDisplayOrientation(90);
+							camera.startPreview();
+							previewing = true;
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		
+		//Stop Preview Button Listener
+		buttonStopCameraPreview.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(camera != null && previewing) {
+					camera.stopPreview();
+					camera.release();
+					camera = null;
+					previewing = false;
+				}
+			}
+		});
+		
+		return view;
 	}
-	
-//	@Override
-//	public void onResume() {
-//		super.onResume();
-//		
-//		camera = Camera.open();
-//		startPreview();
-//	}
-//	
-//	@Override
-//	public void onPause() {
-//		if (inPreview) {
-//			camera.stopPreview();
-//		}
-//		
-//		camera.release();
-//		camera = null;
-//		inPreview = false;
-//		
-//		super.onPause();
-//	}
-//	
-//	private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-//	    Camera.Size result=null;
-//	    
-//	    for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-//	      if (size.width<=width && size.height<=height) {
-//	        if (result==null) {
-//	          result=size;
-//	        }
-//	        else {
-//	          int resultArea=result.width*result.height;
-//	          int newArea=size.width*size.height;
-//	          
-//	          if (newArea>resultArea) {
-//	            result=size;
-//	          }
-//	        }
-//	      }
-//	    }
-//	    
-//	    return(result);
-//	  }
-//	
-//	private void initPreview(int width, int height) {
-//	    if (camera!=null && previewHolder.getSurface()!=null) {
-//	      try {
-//	        camera.setPreviewDisplay(previewHolder);
-//	      }
-//	      catch (Throwable t) {
-//	        Log.e("PreviewDemo-surfaceCallback",
-//	              "Exception in setPreviewDisplay()", t);
-//	        
-//	      }
-//
-//	      if (!cameraConfigured) {
-//	        Camera.Parameters parameters=camera.getParameters();
-//	        Camera.Size size=getBestPreviewSize(width, height,
-//	                                            parameters);
-//	        
-//	        if (size!=null) {
-//	          parameters.setPreviewSize(size.width, size.height);
-//	          camera.setParameters(parameters);
-//	          cameraConfigured=true;
-//	        }
-//	      }
-//	    }
-//	  }
-//	
-//	private void startPreview() {
-//	    if (cameraConfigured && camera!=null) {
-//	      camera.startPreview();
-//	      inPreview=true;
-//	    }
-//	}
-//	
-//	SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
-//	    public void surfaceCreated(SurfaceHolder holder) {
-//	      // no-op -- wait until surfaceChanged()
-//	    }
-//	    
-//	    public void surfaceChanged(SurfaceHolder holder,
-//	                               int format, int width,
-//	                               int height) {
-//	      initPreview(width, height);
-//	      startPreview();
-//	    }
-//	    
-//	    public void surfaceDestroyed(SurfaceHolder holder) {
-//	      // no-op
-//	    }
-//	  };
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
