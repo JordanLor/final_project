cefcfd1cee5cdfd12a1b1d73ec4752f90a35b1b3
Anthony Burch <aburch@gmail.com>
2014-02-08 11:42:31 -0500

    More FB code added to Fragment2

--- Swipe_Fragments/gen/com/example/swipe_fragments/R.java
+++ Swipe_Fragments/gen/com/example/swipe_fragments/R.java
@@ -224,7 +224,7 @@ containing a value of this type.
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f04001c;
-        public static final int action_settings=0x7f040022;
+        public static final int action_settings=0x7f040025;
         public static final int authButton=0x7f040019;
         public static final int com_facebook_login_activity_progress_bar=0x7f040005;
         public static final int com_facebook_picker_activity_circle=0x7f040004;
@@ -248,6 +248,7 @@ containing a value of this type.
         public static final int com_facebook_usersettingsfragment_profile_name=0x7f040016;
         public static final int eraseButton=0x7f040021;
         public static final int filterButton=0x7f040020;
+        public static final int icon=0x7f040022;
         public static final int large=0x7f040002;
         public static final int normal=0x7f040001;
         public static final int photoView=0x7f04001e;
@@ -258,6 +259,8 @@ containing a value of this type.
         public static final int small=0x7f040000;
         public static final int surfaceview=0x7f04001d;
         public static final int takepicture=0x7f040018;
+        public static final int text1=0x7f040023;
+        public static final int text2=0x7f040024;
         public static final int viewpager=0x01010000;
     }
     public static final class layout {
@@ -282,7 +285,8 @@ containing a value of this type.
         public static final int fragment3=0x7f030012;
         public static final int fragment4=0x7f030013;
         public static final int fragment5=0x7f030014;
-        public static final int viewpager_layout=0x7f030015;
+        public static final int listitem=0x7f030015;
+        public static final int viewpager_layout=0x7f030016;
     }
     public static final class menu {
         public static final int main=0x7f090000;
