cb6de6757aad0ec4c852619857028f5966945af5
Anthony Burch <aburch@gmail.com>
2014-03-28 13:03:00 -0400

    Facebook additions

--- Swipe_Fragments/src/com/example/swipe_fragments/Fragment1.java
+++ Swipe_Fragments/src/com/example/swipe_fragments/Fragment1.java
@@ -2,6 +2,9 @@
 
 package com.example.swipe_fragments;
 
+import java.util.List;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.Request;
 import com.facebook.Response;
@@ -136,6 +142,51 @@ public class Fragment1 extends Fragment {
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		uiHelper.onSaveInstanceState(outState);
+	}
+	
+	private class ActionListAdapter extends ArrayAdapter<BaseListElement> {
+	    private List<BaseListElement> listElements;
+
+	    public ActionListAdapter(Context context, int resourceId, 
+	                             List<BaseListElement> listElements) {
+	        super(context, resourceId, listElements);
+	        this.listElements = listElements;
+	        // Set up as an observer for list item changes to
+	        // refresh the view.
+	        for (int i = 0; i < listElements.size(); i++) {
+	            listElements.get(i).setAdapter(this);
+	        }
+	    }
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        View view = convertView;
+	        if (view == null) {
+	            LayoutInflater inflater =
+	                    (LayoutInflater) getActivity()
+	                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	            view = inflater.inflate(R.layout.listitem, null);
+	        }
+
+	        BaseListElement listElement = listElements.get(position);
+	        if (listElement != null) {
+	            view.setOnClickListener(listElement.getOnClickListener());
+	            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+	            TextView text1 = (TextView) view.findViewById(R.id.text1);
+	            TextView text2 = (TextView) view.findViewById(R.id.text2);
+	            if (icon != null) {
+	                icon.setImageDrawable(listElement.getIcon());
+	            }
+	            if (text1 != null) {
+	                text1.setText(listElement.getText1());
+	            }
+	            if (text2 != null) {
+	                text2.setText(listElement.getText2());
+	            }
+	        }
+	        return view;
+	    }
+
 	}
 	
 }
